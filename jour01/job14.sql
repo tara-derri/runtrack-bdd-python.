14cb9e6a6b18bd51efcfa9eaba670dfa393868b7
tara-derri <146070103+tara-derri@users.noreply.github.com>
2024-02-06 17:26:36 +0100

    job14.sql

--- jour01/job14.sql
+++ jour01/job14.sql
@@ -1,3 +1,7 @@
+#récupérer les élèves dont l’âge est compris entre 18 et
+25 ans en triant par l’âge des étudiants par ordre croissant.
+
+
 SELECT * FROM etudiant
 WHERE age BETWEEN 18 AND 25
 ORDER BY age ASC;
