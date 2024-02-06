1a9d17dd08fc8a701b2d83ec96545d7594c2ac1c
tara-derri <146070103+tara-derri@users.noreply.github.com>
2024-02-06 17:31:44 +0100

    job21.sql

--- jour01/job21.sql
+++ jour01/job21.sql
@@ -1,3 +1,6 @@
+#compter le nombre d’étudiants dont l’âge est compris entre
+18 et 25 ans présents en base de données.
+
 mysql> SELECT COUNT(*) AS nombre_etudiants_entre_18_et_25
     -> FROM etudiant
     -> WHERE age BETWEEN 18 AND 25;
